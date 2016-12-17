8469f7347f5a69b1fe650316352f50ce9a2ddc4f
Luke Schuyler <lukeschuyler1@gmail.com>
2016-12-17 13:38:15 -0600

    added more styling and transition

--- quiz.js
+++ quiz.js
@@ -1,6 +1,24 @@
-$('h1').hide();
-$('h1').show('slow');
-console.log(Battle.Dome.Droid)
-
 var newPlayer = new Battle.Dome.Droid()
 console.log(newPlayer)
+
+// INITIAL PAGE HIDES FOR SPA EXECUTION
+
+$('#player-names').hide();
+$('#choose-robot-class').hide();
+$('#choose-robot-type').hide();
+
+$(document).ready(function () {
+	$('#player-names').show('slow');
+})
+
+// CLICK EVENTS FOR PAGE TRANSITIONS
+
+$('#choose-class').click(function (e){
+	$('#player-names').hide('slow');
+	$('#choose-robot-class').show('slow');
+})
+
+$('#choose-robot').click(function (e){
+	$('#choose-robot-class').hide('slow');
+	$('#choose-robot-type').show('slow');
+})
