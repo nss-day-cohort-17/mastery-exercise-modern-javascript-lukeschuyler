e674e1d475c1b4a44c7d146e7a0314227a0ad1e1
Luke Schuyler <lukeschuyler1@gmail.com>
2016-12-17 14:03:08 -0600

    added hides to specific robot types pending what button is clicked

--- quiz.js
+++ quiz.js
@@ -6,6 +6,8 @@ console.log(newPlayer)
 $('#player-names').hide();
 $('#choose-robot-class').hide();
 $('#choose-robot-type').hide();
+$('.robot-types').hide();
+
 
 $(document).ready(function () {
 	$('#player-names').show('slow');
@@ -14,11 +16,29 @@ $(document).ready(function () {
 // CLICK EVENTS FOR PAGE TRANSITIONS
 
 $('#choose-class').click(function (e){
-	$('#player-names').hide('slow');
-	$('#choose-robot-class').show('slow');
+		$('#player-names').hide('slow');
+		$('#choose-robot-class').show('slow');
 })
 
 $('#choose-robot').click(function (e){
 	$('#choose-robot-class').hide('slow');
 	$('#choose-robot-type').show('slow');
+})
+
+$('#droid-button').click(function (e) {
+	$('.android').hide();
+	$('.cyborg').hide();
+	$('.droid').show();
+})
+
+$('#cyborg-button').click(function (e) {
+	$('.droid').hide();
+	$('.android').hide();
+	$('.cyborg').show();
+})
+
+$('#android-button').click(function (e) {
+	$('.cyborg').hide();
+	$('.droid').hide();
+	$('.android').show();
 })
