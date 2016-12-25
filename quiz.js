eabd69dcefce47dc5d48348a31e521c95e4e07b5
Luke Schuyler <lukeschuyler1@gmail.com>
2016-12-24 21:04:51 -0600

    added 2player

--- quiz.js
+++ quiz.js
@@ -4,6 +4,7 @@ var enemyPlayer;
 var enemyPlayerName;
 var enemyInfo;
 var yourInfo;
+var twoPlayer=false;
 
 // JQUERY STYLING 
 
@@ -23,6 +24,8 @@ $('#choose-robot-class-2').hide();
 $('#enter').hide();
 $('#choose-enemy').hide();
 $('#game-over').hide();
+$('#battleground-2').hide();
+// $('#versus-container').hide();
 
 }
 reset();
@@ -31,6 +34,10 @@ $(document).ready(function () {
 	$('#player-names').show('slow');
 	$('#main-header-container').show('slow');
 })
+
+
+// 1 Player 
+
 
 // CHECK IF FIELDS HAVE VALUE
 
@@ -41,6 +48,13 @@ $('.robot-input').keyup(function (e) {
     }
 })
 
+$('#choose-class-2').prop('disabled', true);
+$('.robot-input').keyup(function (e) {
+	if($('#robot1').val() != '' && $('#robot2').val() != '') {
+        $('#choose-class-2').prop('disabled', false);
+    }
+})
+
 // CLICK EVENTS FOR OBJECT INFO ASSIGNING
 
 $('#choose-class').click(function (e) {
@@ -48,6 +62,16 @@ $('#choose-class').click(function (e) {
 	enemyPlayerName = $('#robot2').val();
 	newPlayer = new Battle.Combatant.Robot(newPlayerName);
 	enemyPlayer = new Battle.Combatant.Robot(enemyPlayerName);
+	twoPlayer = false;
+	console.log(twoPlayer)
+})
+
+$('#choose-class-2').click(function (e) {
+	newPlayerName = $('#robot1').val();
+	enemyPlayerName = $('#robot2').val();
+	newPlayer = new Battle.Combatant.Robot(newPlayerName);
+	enemyPlayer = new Battle.Combatant.Robot(enemyPlayerName);
+	twoPlayer = true;
 })
 
 $('.robot-types').click(function (e) {
@@ -61,6 +85,7 @@ $('.class-button-group').click(function (e) {
     var typeText = this.innerText.toLowerCase();
     var newTypeText = typeText.charAt(0).toUpperCase() + typeText.slice(1);
     newPlayer.class = new Battle.Dome[newTypeText];
+  
   })
 
 $('.robot-types-2').click(function (e) {
@@ -68,17 +93,24 @@ $('.robot-types-2').click(function (e) {
     var newTypeText = typeText.charAt(0).toUpperCase() + typeText.slice(1);
     enemyPlayer.type = new Battle.Dome[newTypeText];
     $('#enter').show('slow');
+  
   })
 
-$('.class-button-group').click(function (e) {
+$('.class-button-group-2').click(function (e) {
     var typeText = this.innerText.toLowerCase();
     var newTypeText = typeText.charAt(0).toUpperCase() + typeText.slice(1);
     enemyPlayer.class = new Battle.Dome[newTypeText];
+  
   })
 
 // CLICK EVENTS FOR PAGE TRANSITIONS
 
 $('#choose-class').click(function (e){
+	$('#player-names').hide('slow');
+	$('#choose-robot-class').show('slow');
+})
+
+$('#choose-class-2').click(function (e){
 	$('#player-names').hide('slow');
 	$('#choose-robot-class').show('slow');
 })
@@ -137,6 +169,8 @@ $('#android-button-2').click(function (e) {
 })
 
 $('#enter').click(function (e){
+	if (twoPlayer === false) {
+	console.log(twoPlayer);
 	$('#choose-robot-class-2').hide('slow');
 	$('#main-header-container').hide('slow');
 	
@@ -163,23 +197,58 @@ $('#enter').click(function (e){
 				battle();
 			}, 4000)
 	}, 3000);
+	
+	} else if (twoPlayer === true) {
+		console.log(twoPlayer);
+		console.log('hey');
+		$('#choose-robot-class-2').hide('slow');
+		$('#main-header-container').hide('slow');
+		
+		$('body').fadeTo('slow', 1, function () {
+		$('body').removeClass('player-body');
+		$('body').addClass('battle-body');
+		})
+		
+		$('#vs').html(newPlayer.playerName + ' vs ' + enemyPlayer.playerName)
+		$('#versus').html('<span>GET READY...</span>');
+
+		setTimeout(function () {
+		$('#versus').html('<span>3</span>')
+			setTimeout(function () {
+			$('#versus').html('<span>2</span>')
+			}, 1000);
+			setTimeout(function () {
+				$('#versus').html('<span>1</span>')
+			}, 2000)
+			setTimeout(function () {
+				$('#versus').html('<span>FIGHT!</span>')
+			}, 3000);
+			setTimeout(function () {
+				battleTwo();
+			}, 4000)
+	}, 3000);
+	}
 })
 
 function endGame () {
   if (newPlayer.health <= 0) {
+  	$('body').addClass('overlay');
   	setTimeout(function () {
   		$('#battleground').hide('slow');
+  		$('#battleground-2').hide('slow');
   		$('.game-end').html(`${enemyPlayer.playerName} Wins<br>You Lose`);
   		$('#game-over').show('slow');
-  	}, 1000)
+  	}, 200)
 
 
   } else if (enemyPlayer.health <= 0) {
+  	$('body').addClass('overlay');
   	setTimeout(function () {
   		$('#battleground').hide('slow');
+  		$('#battleground-2').hide('slow');
   		$('.game-end').html(`${newPlayer.playerName} Wins!`);
   		$('#game-over').show('slow');
-  	}, 1000)
+  	}, 200)
   } 
 
   $('#exitBtn').click(function (e){
@@ -188,7 +257,7 @@ function endGame () {
 
 }
 
-$('#attack').click(function (e) {
+function userAttack() {
 	var yourSpeed = (1 / ((newPlayer.CPU + newPlayer.type.CPUBonus) / 100)) * 1000;
 	
 	enemyPlayer.health = enemyPlayer.health - newPlayer.attack;
@@ -198,6 +267,50 @@ $('#attack').click(function (e) {
     setTimeout(function() {
       $('#attack').prop('disabled', false);
     }, yourSpeed)
+}
+
+function playerOneAttack() {
+	var playerOneSpeed = (1 / ((newPlayer.CPU + newPlayer.type.CPUBonus) / 100)) * 1000;
+	
+	enemyPlayer.health = enemyPlayer.health - newPlayer.attack;
+
+    $('#enemyHealth2').val(enemyPlayer.health);
+    $('#attack1').prop('disabled', true);
+    setTimeout(function() {
+      $('#attack1').prop('disabled', false);
+    }, playerOneSpeed)
+    endGame();
+}
+
+function playerTwoAttack() {
+	var playerTwoSpeed = (1 / ((enemyPlayer.CPU + enemyPlayer.type.CPUBonus) / 100)) * 1000;
+	
+	newPlayer.health = newPlayer.health - enemyPlayer.attack;
+
+    $('#health2').val(newPlayer.health);
+    $('#attack2').prop('disabled', true);
+    setTimeout(function() {
+      $('#attack2').prop('disabled', false);
+    }, playerTwoSpeed)
+    endGame();
+}
+
+
+
+$('#attack').click(function (e) {
+
+	userAttack();
+})
+
+$('#attack1').click(function (e) {
+
+	playerOneAttack();
+
+})
+
+$('#attack2').click(function (e) {
+
+	playerTwoAttack();
 
 })
 
@@ -211,11 +324,17 @@ $('#battleground').show('slow');
 $('#health').val(newPlayer.health);
 $('#enemyHealth').val(enemyPlayer.health);
 enemyInfo =`<div id="enemyInfo" class="col-md-4">
-				<span class="enemy-span">Enemy</span><br> Name: ${enemyPlayer.playerName}<br> Type: ${enemyPlayer.type.name} Health: <progress id="enemyHealth" value="${enemyPlayer.health}" max="130"></progress>
+				<span class="enemy-span">Enemy</span><br> 
+				Name: ${enemyPlayer.playerName}<br> 
+				Type: ${enemyPlayer.type.name} Health: 
+				<progress id="enemyHealth" value="${enemyPlayer.health}" max="130"></progress>
 			</div>`
 
 yourInfo = `<div id="yourInfo" class="col-md-4">
-				<span class="enemy-span">You</span><br> Name: ${newPlayer.playerName}<br> Type: ${newPlayer.type.name} Health: <progress id="health" value="${newPlayer.health}" max="130"></progress>
+				<span class="enemy-span">You</span><br> 
+				Name: ${newPlayer.playerName}<br> 
+				Type: ${newPlayer.type.name} Health: 
+				<progress id="health" value="${newPlayer.health}" max="130"></progress>
 			</div>`
 
 $('#battle-screen-top').html(enemyInfo);
@@ -237,7 +356,7 @@ function enemyAttack() {
 
 $('#health').val(newPlayer.health);
 
-// console.log(newPlayer.health)
+
 }
 
 (function loop() {
@@ -253,4 +372,38 @@ $('#health').val(newPlayer.health);
 
 }());
 
+$(document).keypress(function (e) { 
+	if (e.keyCode === 32) {
+		userAttack();
+  	}	
+})
+
+}
+
+function battleTwo () {
+newPlayer.health = newPlayer.health + newPlayer.type.healthBonus;
+enemyPlayer.health = enemyPlayer.health + enemyPlayer.type.healthBonus;
+$('#versus-container').hide()
+$('#battleground-2').show('slow');
+$('#health').val(newPlayer.health);
+$('#enemyHealth').val(enemyPlayer.health);
+enemyInfo =`<div id="enemyInfo" class="col-md-4">
+				<span class="enemy-span">Player 2</span><br> 
+				Name: ${enemyPlayer.playerName}<br> 
+				Type: ${enemyPlayer.type.name} Health: 
+				<progress id="enemyHealth2" value="${enemyPlayer.health}" max="130"></progress>
+			</div>`
+
+yourInfo = `<div id="yourInfo" class="col-md-4">
+				<span class="enemy-span">Player 1</span><br> 
+				Name: ${newPlayer.playerName}<br> 
+				Type: ${newPlayer.type.name} Health: 
+				<progress id="health2" value="${newPlayer.health}" max="130"></progress>
+			</div>`
+
+
+$('#battle-screen-bottom-1').html(yourInfo)
+$('#battle-screen-bottom-2').html(enemyInfo)
+
+endGame();
 }
